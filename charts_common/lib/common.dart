e63aef54bfd462b29d1cac06bcf7bdc9e4441ef5
Nicholas Shahan <nshahan@google.com>
2018-03-23 13:35:24 -0700

    GitHub Sync (#22)
    * Update style imports for general use
    
    PiperOrigin-RevId: 190232098
    
    * Add Pub badges to the readme files
    
    Similar to the nice blue one here:
    https://pub.dartlang.org/packages/url_launcher
    
    Closes #17
    
    PiperOrigin-RevId: 190236267
    
    * Remove unused files from example
    
    PiperOrigin-RevId: 190253912
    
    * Copy LICENSE file to the package sub-directories
    
    PiperOrigin-RevId: 190259764
    
    * Update the front page of the docs website
    
    Closes #18
    
    PiperOrigin-RevId: 190265508

--- charts_common/lib/common.dart
+++ charts_common/lib/common.dart
@@ -13,25 +13,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export 'src/chart/common/behavior/chart_behavior.dart' show ChartBehavior;
+export 'src/chart/bar/bar_chart.dart' show BarChart;
+export 'src/chart/bar/bar_label_decorator.dart' show BarLabelDecorator;
+export 'src/chart/bar/bar_renderer.dart'
+    show BarRenderer, ImmutableBarRendererElement;
+export 'src/chart/bar/bar_renderer_config.dart' show BarRendererConfig;
+export 'src/chart/bar/bar_renderer_decorator.dart' show BarRendererDecorator;
+export 'src/chart/bar/base_bar_renderer_config.dart'
+    show BarGroupingType, BaseBarRendererConfig;
+export 'src/chart/cartesian/axis/axis.dart' show domainAxisKey, measureAxisKey;
+export 'src/chart/cartesian/axis/spec/axis_spec.dart' show AxisSpec;
+export 'src/chart/cartesian/cartesian_chart.dart' show CartesianChart;
+export 'src/chart/cartesian/cartesian_renderer.dart' show BaseCartesianRenderer;
+export 'src/chart/common/base_chart.dart' show BaseChart, LifecycleListener;
 export 'src/chart/common/behavior/a11y/a11y_explore_behavior.dart'
     show ExploreModeTrigger;
 export 'src/chart/common/behavior/a11y/a11y_node.dart' show A11yNode;
 export 'src/chart/common/behavior/a11y/domain_a11y_explore_behavior.dart'
     show DomainA11yExploreBehavior, VocalizationCallback;
-export 'src/chart/common/behavior/zoom/pan_and_zoom_behavior.dart'
-    show PanAndZoomBehavior;
-export 'src/chart/common/behavior/zoom/pan_behavior.dart' show PanBehavior;
-export 'src/chart/common/chart_canvas.dart' show ChartCanvas, FillPatternType;
-export 'src/chart/common/base_chart.dart' show BaseChart, LifecycleListener;
-export 'src/chart/common/canvas_shapes.dart'
-    show CanvasBarStack, CanvasPie, CanvasPieSlice, CanvasRect;
-export 'src/chart/common/chart_context.dart' show ChartContext;
-export 'src/chart/common/datum_details.dart'
-    show DatumDetails, DomainFormatter, MeasureFormatter;
-export 'src/chart/common/series_renderer.dart' show SeriesRenderer;
-export 'src/chart/common/series_renderer_config.dart'
-    show RendererAttributeKey, SeriesRendererConfig;
 export 'src/chart/common/behavior/chart_behavior.dart' show ChartBehavior;
 export 'src/chart/common/behavior/domain_highlighter.dart'
     show DomainHighlighter;
@@ -44,43 +43,22 @@ export 'src/chart/common/behavior/range_annotation.dart'
     show RangeAnnotation, RangeAnnotationAxisType, RangeAnnotationSegment;
 export 'src/chart/common/behavior/select_nearest.dart'
     show SelectNearest, SelectNearestTrigger;
+export 'src/chart/common/behavior/zoom/pan_and_zoom_behavior.dart'
+    show PanAndZoomBehavior;
+export 'src/chart/common/behavior/zoom/pan_behavior.dart' show PanBehavior;
+export 'src/chart/common/canvas_shapes.dart'
+    show CanvasBarStack, CanvasPie, CanvasPieSlice, CanvasRect;
+export 'src/chart/common/chart_canvas.dart' show ChartCanvas, FillPatternType;
+export 'src/chart/common/chart_context.dart' show ChartContext;
+export 'src/chart/common/datum_details.dart'
+    show DatumDetails, DomainFormatter, MeasureFormatter;
 export 'src/chart/common/selection_model/selection_model.dart'
     show SelectionModel, SelectionModelType, SelectionModelListener;
-export 'src/chart/layout/layout_view.dart' show LayoutPosition, ViewMargin;
+export 'src/chart/common/series_renderer.dart' show SeriesRenderer;
+export 'src/chart/common/series_renderer_config.dart'
+    show RendererAttributeKey, SeriesRendererConfig;
 export 'src/chart/layout/layout_config.dart' show LayoutConfig, MarginSpec;
-export 'src/common/color.dart' show Color;
-export 'src/common/date_time_factory.dart'
-    show DateTimeFactory, LocalDateTimeFactory, UTCDateTimeFactory;
-export 'src/common/gesture_listener.dart' show GestureListener;
-export 'src/common/graphics_factory.dart' show GraphicsFactory;
-export 'src/common/material_palette.dart' show MaterialPalette;
-export 'src/common/performance.dart' show Performance;
-export 'src/common/proxy_gesture_listener.dart' show ProxyGestureListener;
-export 'src/common/rtl_spec.dart' show RTLSpec, AxisPosition;
-export 'src/common/line_style.dart' show LineStyle;
-export 'src/common/symbol_renderer.dart'
-    show SymbolRenderer, RoundedRectSymbolRenderer;
-export 'src/common/style/style_factory.dart' show StyleFactory;
-export 'src/common/style/material_style.dart' show MaterialStyle;
-export 'src/common/style/quantum_style.dart' show QuantumStyle;
-export 'src/common/text_element.dart'
-    show TextElement, TextDirection, MaxWidthStrategy;
-export 'src/common/text_measurement.dart' show TextMeasurement;
-export 'src/common/text_style.dart' show TextStyle;
-export 'src/common/quantum_palette.dart' show QuantumPalette;
-export 'src/data/series.dart' show Series;
-export 'src/chart/bar/bar_chart.dart' show BarChart;
-export 'src/chart/bar/bar_renderer.dart'
-    show BarRenderer, ImmutableBarRendererElement;
-export 'src/chart/bar/bar_renderer_config.dart' show BarRendererConfig;
-export 'src/chart/bar/bar_renderer_decorator.dart' show BarRendererDecorator;
-export 'src/chart/bar/bar_label_decorator.dart' show BarLabelDecorator;
-export 'src/chart/bar/base_bar_renderer_config.dart'
-    show BarGroupingType, BaseBarRendererConfig;
-export 'src/chart/cartesian/axis/axis.dart' show domainAxisKey, measureAxisKey;
-export 'src/chart/cartesian/axis/spec/axis_spec.dart' show AxisSpec;
-export 'src/chart/cartesian/cartesian_chart.dart' show CartesianChart;
-export 'src/chart/cartesian/cartesian_renderer.dart' show BaseCartesianRenderer;
+export 'src/chart/layout/layout_view.dart' show LayoutPosition, ViewMargin;
 export 'src/chart/line/line_chart.dart' show LineChart;
 export 'src/chart/line/line_renderer.dart' show LineRenderer;
 export 'src/chart/line/line_renderer_config.dart' show LineRendererConfig;
@@ -88,3 +66,22 @@ export 'src/chart/pie/arc_renderer.dart' show ArcRenderer;
 export 'src/chart/pie/arc_renderer_config.dart' show ArcRendererConfig;
 export 'src/chart/pie/pie_chart.dart' show PieChart;
 export 'src/chart/time_series/time_series_chart.dart' show TimeSeriesChart;
+export 'src/common/color.dart' show Color;
+export 'src/common/date_time_factory.dart'
+    show DateTimeFactory, LocalDateTimeFactory, UTCDateTimeFactory;
+export 'src/common/gesture_listener.dart' show GestureListener;
+export 'src/common/graphics_factory.dart' show GraphicsFactory;
+export 'src/common/line_style.dart' show LineStyle;
+export 'src/common/material_palette.dart' show MaterialPalette;
+export 'src/common/performance.dart' show Performance;
+export 'src/common/proxy_gesture_listener.dart' show ProxyGestureListener;
+export 'src/common/rtl_spec.dart' show RTLSpec, AxisPosition;
+export 'src/common/style/material_style.dart' show MaterialStyle;
+export 'src/common/style/style_factory.dart' show StyleFactory;
+export 'src/common/symbol_renderer.dart'
+    show SymbolRenderer, RoundedRectSymbolRenderer;
+export 'src/common/text_element.dart'
+    show TextElement, TextDirection, MaxWidthStrategy;
+export 'src/common/text_measurement.dart' show TextMeasurement;
+export 'src/common/text_style.dart' show TextStyle;
+export 'src/data/series.dart' show Series;
