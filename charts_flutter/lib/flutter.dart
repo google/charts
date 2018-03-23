0d4821a5ffdb0fef59eafdabeebe50b01380e05c
nshahan <nshahan@google.com>
2018-03-23 09:46:02 -0700

    Update style imports for general use
    PiperOrigin-RevId: 190232098

--- charts_flutter/lib/flutter.dart
+++ charts_flutter/lib/flutter.dart
@@ -13,62 +13,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export 'package:charts_common/src/chart/common/base_chart.dart' show BaseChart;
-export 'package:charts_common/src/chart/layout/layout_view.dart'
-    show LayoutPosition, ViewMargin;
-export 'package:charts_common/src/chart/layout/layout_config.dart'
-    show MarginSpec;
-export 'package:charts_common/src/chart/common/chart_canvas.dart'
-    show ChartCanvas, FillPatternType;
-export 'package:charts_common/src/chart/common/processed_series.dart'
-    show SeriesDatum, ImmutableSeries;
-export 'package:charts_common/src/chart/common/selection_model/selection_model.dart'
-    show SelectionModelType, SelectionModel, SelectionModelListener;
-export 'package:charts_common/src/chart/common/chart_context.dart'
-    show ChartContext;
-export 'package:charts_common/src/chart/common/behavior/chart_behavior.dart'
-    show ChartBehavior;
-export 'package:charts_common/src/chart/common/behavior/range_annotation.dart'
-    show RangeAnnotationAxisType, RangeAnnotationSegment;
-export 'package:charts_common/src/chart/common/behavior/select_nearest.dart'
-    show SelectNearestTrigger;
-export 'package:charts_common/src/chart/common/behavior/a11y/a11y_explore_behavior.dart'
-    show ExploreModeTrigger;
-export 'package:charts_common/src/chart/common/behavior/a11y/domain_a11y_explore_behavior.dart'
-    show VocalizationCallback;
-export 'package:charts_common/src/common/color.dart' show Color;
-export 'package:charts_common/src/common/gesture_listener.dart'
-    show GestureListener;
-export 'package:charts_common/src/common/material_palette.dart'
-    show MaterialPalette;
-export 'package:charts_common/src/common/performance.dart' show Performance;
-export 'package:charts_common/src/common/date_time_factory.dart'
-    show DateTimeFactory, LocalDateTimeFactory, UTCDateTimeFactory;
-export 'package:charts_common/src/common/rtl_spec.dart'
-    show RTLSpec, AxisPosition;
-export 'package:charts_common/src/common/quantum_palette.dart'
-    show QuantumPalette;
-export 'package:charts_common/src/common/style/style_factory.dart'
-    show StyleFactory;
-export 'package:charts_common/src/common/style/material_style.dart'
-    show MaterialStyle;
-export 'package:charts_common/src/common/style/quantum_style.dart'
-    show QuantumStyle;
-export 'package:charts_common/src/data/series.dart' show Series;
-export 'package:charts_common/src/chart/bar/bar_renderer_config.dart'
-    show BarRendererConfig;
 export 'package:charts_common/src/chart/bar/bar_label_decorator.dart'
     show BarLabelDecorator, BarLabelAnchor, BarLabelPosition;
+export 'package:charts_common/src/chart/bar/bar_renderer_config.dart'
+    show BarRendererConfig;
 export 'package:charts_common/src/chart/bar/bar_target_line_renderer_config.dart'
     show BarTargetLineRendererConfig;
 export 'package:charts_common/src/chart/bar/base_bar_renderer_config.dart'
     show BarGroupingType;
-export 'package:charts_common/src/chart/cartesian/cartesian_chart.dart'
-    show CartesianChart;
 export 'package:charts_common/src/chart/cartesian/axis/axis.dart'
     show measureAxisIdKey;
+export 'package:charts_common/src/chart/cartesian/axis/draw_strategy/gridline_draw_strategy.dart'
+    show GridlineRendererSpec;
+export 'package:charts_common/src/chart/cartesian/axis/draw_strategy/none_draw_strategy.dart'
+    show NoneRenderSpec;
+export 'package:charts_common/src/chart/cartesian/axis/draw_strategy/small_tick_draw_strategy.dart'
+    show SmallTickRendererSpec;
 export 'package:charts_common/src/chart/cartesian/axis/spec/axis_spec.dart'
     show TickLabelAnchor, TickLabelJustification, TextStyleSpec, LineStyleSpec;
+export 'package:charts_common/src/chart/cartesian/axis/spec/date_time_axis_spec.dart'
+    show
+        DateTimeAxisSpec,
+        AutoDateTimeTickFormatterSpec,
+        AutoDateTimeTickProviderSpec,
+        TimeFormatterSpec,
+        StaticDateTimeTickProviderSpec;
 export 'package:charts_common/src/chart/cartesian/axis/spec/numeric_axis_spec.dart'
     show
         NumericAxisSpec,
@@ -81,44 +50,71 @@ export 'package:charts_common/src/chart/cartesian/axis/spec/ordinal_axis_spec.da
         OrdinalTickFormatterSpec,
         OrdinalTickProviderSpec,
         StaticOrdinalTickProviderSpec;
-export 'package:charts_common/src/chart/cartesian/axis/spec/date_time_axis_spec.dart'
-    show
-        DateTimeAxisSpec,
-        AutoDateTimeTickFormatterSpec,
-        AutoDateTimeTickProviderSpec,
-        TimeFormatterSpec,
-        StaticDateTimeTickProviderSpec;
 export 'package:charts_common/src/chart/cartesian/axis/spec/tick_spec.dart'
     show TickSpec;
-export 'package:charts_common/src/chart/cartesian/axis/draw_strategy/small_tick_draw_strategy.dart'
-    show SmallTickRendererSpec;
-export 'package:charts_common/src/chart/cartesian/axis/draw_strategy/gridline_draw_strategy.dart'
-    show GridlineRendererSpec;
-export 'package:charts_common/src/chart/cartesian/axis/draw_strategy/none_draw_strategy.dart'
-    show NoneRenderSpec;
+export 'package:charts_common/src/chart/cartesian/cartesian_chart.dart'
+    show CartesianChart;
+export 'package:charts_common/src/chart/common/base_chart.dart' show BaseChart;
+export 'package:charts_common/src/chart/common/behavior/a11y/a11y_explore_behavior.dart'
+    show ExploreModeTrigger;
+export 'package:charts_common/src/chart/common/behavior/a11y/domain_a11y_explore_behavior.dart'
+    show VocalizationCallback;
+export 'package:charts_common/src/chart/common/behavior/chart_behavior.dart'
+    show ChartBehavior;
+export 'package:charts_common/src/chart/common/behavior/range_annotation.dart'
+    show RangeAnnotationAxisType, RangeAnnotationSegment;
+export 'package:charts_common/src/chart/common/behavior/select_nearest.dart'
+    show SelectNearestTrigger;
+export 'package:charts_common/src/chart/common/chart_canvas.dart'
+    show ChartCanvas, FillPatternType;
+export 'package:charts_common/src/chart/common/chart_context.dart'
+    show ChartContext;
+export 'package:charts_common/src/chart/common/processed_series.dart'
+    show SeriesDatum, ImmutableSeries;
+export 'package:charts_common/src/chart/common/selection_model/selection_model.dart'
+    show SelectionModelType, SelectionModel, SelectionModelListener;
+export 'package:charts_common/src/chart/common/series_renderer.dart'
+    show rendererIdKey;
+export 'package:charts_common/src/chart/layout/layout_config.dart'
+    show MarginSpec;
+export 'package:charts_common/src/chart/layout/layout_view.dart'
+    show LayoutPosition, ViewMargin;
 export 'package:charts_common/src/chart/line/line_renderer_config.dart'
     show LineRendererConfig;
 export 'package:charts_common/src/chart/pie/arc_renderer_config.dart'
     show ArcRendererConfig;
-export 'package:charts_common/src/chart/common/series_renderer.dart'
-    show rendererIdKey;
+export 'package:charts_common/src/common/color.dart' show Color;
+export 'package:charts_common/src/common/date_time_factory.dart'
+    show DateTimeFactory, LocalDateTimeFactory, UTCDateTimeFactory;
+export 'package:charts_common/src/common/gesture_listener.dart'
+    show GestureListener;
+export 'package:charts_common/src/common/material_palette.dart'
+    show MaterialPalette;
+export 'package:charts_common/src/common/performance.dart' show Performance;
+export 'package:charts_common/src/common/rtl_spec.dart'
+    show RTLSpec, AxisPosition;
+export 'package:charts_common/src/common/style/material_style.dart'
+    show MaterialStyle;
+export 'package:charts_common/src/common/style/style_factory.dart'
+    show StyleFactory;
+export 'package:charts_common/src/data/series.dart' show Series;
 
 export 'src/bar_chart.dart';
 export 'src/base_chart.dart' show LayoutConfig;
-export 'src/line_chart.dart';
-export 'src/pie_chart.dart';
-export 'src/time_series_chart.dart';
-export 'src/selection_model_config.dart' show SelectionModelConfig;
+export 'src/behaviors/a11y/domain_a11y_explore_behavior.dart'
+    show DomainA11yExploreBehavior;
 export 'src/behaviors/chart_behavior.dart'
     show BuildablePosition, OutsideJustification, InsideJustification;
 export 'src/behaviors/domain_highlighter.dart' show DomainHighlighter;
+export 'src/behaviors/legend/series_legend.dart' show SeriesLegend;
 export 'src/behaviors/line_point_highlighter.dart' show LinePointHighlighter;
 export 'src/behaviors/range_annotation.dart' show RangeAnnotation;
 export 'src/behaviors/select_nearest.dart' show SelectNearest;
-export 'src/behaviors/a11y/domain_a11y_explore_behavior.dart'
-    show DomainA11yExploreBehavior;
-export 'src/behaviors/legend/series_legend.dart' show SeriesLegend;
 export 'src/behaviors/zoom/pan_and_zoom_behavior.dart' show PanAndZoomBehavior;
 export 'src/behaviors/zoom/pan_behavior.dart' show PanBehavior;
+export 'src/line_chart.dart';
+export 'src/pie_chart.dart';
+export 'src/selection_model_config.dart' show SelectionModelConfig;
 export 'src/symbol_renderer.dart'
     show SymbolRenderer, RoundedRectSymbolRenderer;
+export 'src/time_series_chart.dart';
