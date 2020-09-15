9cbde0a9c5b55fcaacfc2cc74fe74cc5ebf62064
Googler <noreply@google.com>
2020-09-15 07:37:24 -0700

    Internal changes
    PiperOrigin-RevId: 331765706

--- charts_common/lib/common.dart
+++ charts_common/lib/common.dart
@@ -258,5 +258,6 @@ export 'src/common/gesture_listener.dart' show GestureListener;
 export 'src/common/graphics_factory.dart' show GraphicsFactory;
 export 'src/common/line_style.dart' show LineStyle;
 export 'src/common/material_palette.dart' show MaterialPalette;
+export 'src/common/palette.dart' show Palette;
 export 'src/common/performance.dart' show Performance;
 export 'src/common/proxy_gesture_listener.dart' show ProxyGestureListener;
