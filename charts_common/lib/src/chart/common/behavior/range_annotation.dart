df9a9f650341f7ef4206379393a0ae4693087ff1
Googler <noreply@google.com>
2021-04-26 18:57:12 -0700

    Fix build warnings in charts_common
    PiperOrigin-RevId: 370588847

--- charts_common/lib/src/chart/common/behavior/range_annotation.dart
+++ charts_common/lib/src/chart/common/behavior/range_annotation.dart
@@ -235,7 +235,7 @@ class RangeAnnotation<D> implements ChartBehavior<D> {
           ? annotation.dashPattern
           : null;
       final strokeWidthPx = annotation is LineAnnotationSegment<Object>
-          ? annotation.strokeWidthPx ?? defaultStrokeWidthPx
+          ? annotation.strokeWidthPx
           : 0.0;
 
       final isRange = annotation is RangeAnnotationSegment;
