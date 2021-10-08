78d946c6b496d972248faa458f424e6c57489f9b
pinkfish <pinkfishfrog@gmail.com>
2021-10-08 05:18:46 +0000

    Change bits
    Bing

--- charts_flutter/example/ios/Flutter/flutter_export_environment.sh
+++ charts_flutter/example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/david/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/david/development/charts/charts_flutter/example"
+export "FLUTTER_ROOT=/home/pinkfishfrog/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/pinkfishfrog/Documents/GitHub/charts/charts_flutter/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/david/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
